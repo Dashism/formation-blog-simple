34381e4bd11b8cac6a7d690514a4a42a049bd995
Jérémy <elvynia@gmail.com>
2019-01-09 16:49:13 +0100

    Ajout de la modification d'un article.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,25 +4,25 @@ import { NgForm } from '@angular/forms';
 
 const LIST_ARTICLES: Array<Article> = [
   {
-      "id": 0,
-      "title": "Article n°1",
-      "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
-  },{
-      "id": 1,
-      "title": "Article n°2",
-      "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
-  },{
-      "id": 2,
-      "title": "Article n°3",
-      "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
-  },{
-      "id": 3,
-      "title": "Article n°4",
-      "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
-  },{
-      "id": 4,
-      "title": "Article n°5",
-      "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
+    "id": 0,
+    "title": "Article n°1",
+    "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
+  }, {
+    "id": 1,
+    "title": "Article n°2",
+    "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
+  }, {
+    "id": 2,
+    "title": "Article n°3",
+    "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
+  }, {
+    "id": 3,
+    "title": "Article n°4",
+    "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
+  }, {
+    "id": 4,
+    "title": "Article n°5",
+    "content": "Nunc consectetur convallis magna, vitae ornare enim laoreet a. Nulla facilisi. Pellentesque in arcu dapibus sapien vulputate tempor vel id magna. Curabitur in pulvinar augue. Fusce vitae volutpat mi. Curabitur lacinia placerat nulla ac suscipit. Etiam a facilisis magna. Morbi aliquam sem et vehicula fermentum. Morbi sit amet lobortis justo. Sed viverra dui id enim eleifend, ut aliquam velit finibus. Fusce vitae lorem tortor. Pellentesque vitae quam in velit ultrices convallis. Donec non est imperdiet, vulputate felis ut, luctus ipsum. Pellentesque lacinia non leo vitae efficitur."
   }
 ];
 
@@ -35,6 +35,7 @@ export class AppComponent {
   title: string;
   articles: Array<Article>;
   isList: boolean;
+  isEdit: boolean;
   modelArticle: Article;
 
   constructor() {
@@ -42,6 +43,7 @@ export class AppComponent {
     this.articles = new Array();
     this.articles.push(...LIST_ARTICLES);
     this.isList = true;
+    this.isEdit = false;
     this.modelArticle = new Article('', '');
   }
 
@@ -50,14 +52,31 @@ export class AppComponent {
   }
 
   validateForm(myForm: NgForm) {
-    // Ajout du nouvel article à la liste.
-    // Attention : il faut séparer les instances
-    // entre l'objet dans la liste et l'objet
-    // dans le formulaire.
-    this.articles.push(
-      new Article(this.modelArticle.title,
-        this.modelArticle.content,
-        this.modelArticle.id));
+    // On construit une nouvelle instance pour l'article créé ou modifié.
+    // Attention : il faut séparer les instances entre l'objet dans la liste
+    // et l'objet dans le formulaire.
+    const newArticle = new Article(this.modelArticle.title,
+      this.modelArticle.content,
+      this.modelArticle.id);
+    // Si on est en édition.
+    if (this.isEdit) {
+      // Je recherche ou est l'article à remplacer dans le tableau.
+      const index = this.articles.findIndex(
+        (article: Article) => article.id === newArticle.id
+      );
+      // Si l'article est trouvé, c'est à dire si l'index obtenu
+      // ne vaut pas -1 mais une valeur positive.
+      if (index >= 0) {
+        // Le 3eme argument de splice est le nouvel élément à mettre
+        // à la place de celui qui est supprimé.
+        this.articles.splice(index, 1, newArticle);
+      }
+      // On repasse en mode création.
+      this.isEdit = false;
+    } else {
+      // Sinon ajout du nouvel article à la liste.
+      this.articles.push(newArticle);
+    }
     myForm.resetForm(new Article('', ''));
   }
 
@@ -68,5 +87,14 @@ export class AppComponent {
     if (index >= 0) {
       this.articles.splice(index, 1);
     }
+  }
+
+  edit(article: Article) {
+    // On passe en mode édition.
+    this.isEdit = true;
+    // On charge l'article dans le formulaire.
+    this.modelArticle = new Article(article.title, article.content, article.id);
+    // On bascule vers l'affichage du form.
+    this.swapDisplay();
   }
 }
